674071b7baf1f97406dade10501e210ea2950fae
Max Smith <megamaxbsmith@gmail.com>
2022-05-02 17:53:48 -0400

    updates

--- src/components/Art.js
+++ src/components/Art.js
@@ -4,6 +4,7 @@ import '../styles/Art.css';
 const Art = props => {
   return (
     <div className="checkBackSoon">
+      {/* WORK ON ART GALLERY PAGE */}
       <h2>C H E C K &nbsp; B A C K &nbsp; S O O N</h2>
       <p>I haven't finished working on this page yet!</p>
     </div>
