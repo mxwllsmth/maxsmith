48e5f08b6ffc6f9489a8c91e1dc2f864ebb9bd68
Max Smith <megamaxbsmith@gmail.com>
2022-05-03 19:16:47 -0400

    v 2.0 updates

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,17 +1,158 @@
 import React from 'react';
 import '../styles/Art.css';
+import spaceFace from '../images/Space Face Print.jpg';
+import astroFeceic from '../images/Astro Faceic Print.jpg';
+import babaTequila from '../images/Baba Tequila Print.jpg';
+import cogito from '../images/Cogito Print.jpg';
+import hydrospace from '../images/Hydrospace Print.jpg';
+import one from '../images/Mindscape 1 Print.jpg';
+import plur from '../images/P.L.U.R. Print.jpg';
+import portrait from '../images/Portrait.jpg';
+import purpleRain from '../images/Purple Rain Print.jpg';
+import voidArt from '../images/Void Print.jpg';
 
 const Art = props => {
   return (
-    <div>
-      <h2 className="artSubHeader animated">W e l c o m e &nbsp; t o &nbsp; m y &nbsp; a r t &nbsp; g a l l e r y</h2>
-      <div className="checkBackSoon">
+    <div className="checkBackSoon">
       <h2>C H E C K &nbsp; B A C K &nbsp; S O O N</h2>
       <p>I haven't finished working on this page yet!</p>
     </div>
-      <div className="artwork">
-      </div>
-    </div>
+
+
+    // <div>
+    //   <h2 className="artSubHeader animated">W e l c o m e &nbsp; t o &nbsp; m y &nbsp; a r t &nbsp; g a l l e r y</h2>
+
+    //   <div className="artwork">
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">S p a c e &nbsp; F a c e</h3>
+    //       <p className="date">????</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //     <div>
+    //       <img src={spaceFace} id="spaceFace" alt="Artwork"/>
+    //     </div>
+    //   </div>
+
+    //   <div className="artwork">
+    //     <div>
+    //       <img src={cogito} id="cogito" alt="Artwork"/>
+    //     </div>
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">C o g i t o</h3>
+    //       <p className="date">????</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //   </div>
+
+    //   <div className="artwork">
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">A s t r o &nbsp; F a c e i c</h3>
+    //       <p className="date">????</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //     <div>
+    //       <img src={astroFeceic} id="astroFaceic" alt="Artwork"/>
+    //     </div>
+    //   </div>
+
+    //   <div className="artwork">
+    //     <div>
+    //       <img src={hydrospace} id="hydrospace" alt="Artwork"/>
+    //     </div>
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">H y d r o s p a c e</h3>
+    //       <p className="date">????</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //   </div>
+
+    //   <div className="artwork">
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">U n t i t l e d</h3>
+    //       <p className="date">year</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //     <div>
+    //       <img src={portrait} id="portrait" alt="Artwork"/>
+    //     </div>
+    //   </div>
+
+    //   <div className="artwork">
+    //     <div>
+    //       <img src={voidArt} id="voidArt" alt="Artwork"/>
+    //     </div>
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">V o i d</h3>
+    //       <p className="date">????</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //   </div>
+
+    //   <div className="artwork">
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">P . L . U . R .</h3>
+    //       <p className="date">????</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //     <div>
+    //       <img src={plur} id="plur" alt="Artwork"/>
+    //     </div>
+    //   </div>
+
+    //   <div className="artwork">
+    //     <div>
+    //       <img src={babaTequila} id="babaTequila" alt="Artwork"/>
+    //     </div>
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">B a b a &nbsp; T e q u i l a</h3>
+    //       <p className="date">year</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //   </div>
+
+    //   <div className="artwork">
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">O n e</h3>
+    //       <p className="date">year</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //     <div>
+    //       <img src={one} id="one" alt="Artwork"/>
+    //     </div>
+    //   </div>
+
+    //   <div className="artwork">
+    //     <div>
+    //       <img src={purpleRain} id="purpleRain" alt="Artwork"/>
+    //     </div>
+    //     <div className="artInfo">
+    //       <h3 className="artTitle">P u r p l e &nbsp; R a i n</h3>
+    //       <p className="date">year</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //       <p>Description Description Description Description Description Description Description</p>
+    //     </div>
+    //   </div>
+
+    // </div>
   );
 };
 
