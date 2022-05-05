9637bd1322fd0c88893afeb3b632cc1b799f3229
Max Smith <megamaxbsmith@gmail.com>
2022-05-05 13:31:53 -0400

    Flipped education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,29 +7,7 @@ const Education = props => {
   return (
     <div>
 
-      <div id="stocktonContainer">
-        <div id="stocktonU">
-          <div id="subHeader">
-          <a href="https://stockton.edu/" target="_blank" rel="noopener noreferrer"><h2 id="title">Stockton University</h2></a>
-            <h4>2018 - 2020</h4>
-          </div>
-          <p className="smallLine">I attended Stockton University for three years in pursuit of a carer that felt right for me. Over my time there, I changed my major three times.</p>
-          <p>My first major was philosophy, and although I decided early on that I no longer wanted to pursue teaching at the collegiate level and changed my major,
-          my interests in the subject persisted, and I took philosophy classes each semester throughout my time there. Over the next two years I took classes in
-          environmental science, political science, and studio art in search of a field I was excited to work in, but as much as I loved learning about these subjects,
-          I couldn't imagine myself doing the related work. Now, finally, I've found a career path that I'm excited to be working in - software development. 
-          Working in software development employs the skills that I learned through studying philosophy, along with many other things I learned during my time 
-          at Stockton, and I couldn't be happier to see my past experience coming through in my work.</p>
-        </div>
-        <div id="img"> 
-        <a href="https://stockton.edu/" target="_blank" rel="noopener noreferrer"><img src={StocktonLogo} id="stocktonLogo" alt="Stockton University Emblem"/></a>
-        </div>
-      </div>
-
       <div id="teContainer">
-        <div id="img"> 
-        <a href="https://www.techelevator.com/" target="_blank" rel="noopener noreferrer"><img src={TELogo} id="teLogo" alt="Tech Elevator Emblem"/></a>
-        </div>
         <div id="techE">
           <div id="subHeader">
             <a href="https://www.techelevator.com/" target="_blank" rel="noopener noreferrer"><h2 id="title">Tech Elevator</h2></a>
@@ -54,6 +32,28 @@ const Education = props => {
             <li>pg Admin4</li>
           </div>
           <p className="smallLine">In addition, this program provided valuable experience in pair and mob programming as well as debugging and troubleshooting.</p>
+        </div>
+        <div id="img"> 
+          <a href="https://www.techelevator.com/" target="_blank" rel="noopener noreferrer"><img src={TELogo} id="teLogo" alt="Tech Elevator Emblem"/></a>
+        </div>
+      </div>
+
+      <div id="stocktonContainer">
+        <div id="img"> 
+        <a href="https://stockton.edu/" target="_blank" rel="noopener noreferrer"><img src={StocktonLogo} id="stocktonLogo" alt="Stockton University Emblem"/></a>
+        </div>
+        <div id="stocktonU">
+          <div id="subHeader">
+          <a href="https://stockton.edu/" target="_blank" rel="noopener noreferrer"><h2 id="title">Stockton University</h2></a>
+            <h4>2018 - 2020</h4>
+          </div>
+          <p className="smallLine">I attended Stockton University for three years in pursuit of a carer that felt right for me. Over my time there, I changed my major three times.</p>
+          <p>My first major was philosophy, and although I decided early on that I no longer wanted to pursue teaching at the collegiate level and changed my major,
+          my interests in the subject persisted, and I took philosophy classes each semester throughout my time there. Over the next two years I took classes in
+          environmental science, political science, and studio art in search of a field I was excited to work in, but as much as I loved learning about these subjects,
+          I couldn't imagine myself doing the related work. Now, finally, I've found a career path that I'm excited to be working in - software development. 
+          Working in software development employs the skills that I learned through studying philosophy, along with many other things I learned during my time 
+          at Stockton, and I couldn't be happier to see my past experience coming through in my work.</p>
         </div>
       </div>
 
