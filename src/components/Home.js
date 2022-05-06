be4fe469b2ccedbe7113d183459c21feb3d5abd7
Max Smith <megamaxbsmith@gmail.com>
2022-05-06 17:54:02 -0400

    Landing page responsive design update

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,15 +10,15 @@ const Home = props => {
       <div id="homeContainer">
         <h1 id="welcome">W e l c o m e</h1>
         <div id="homeAbout">
-          <p className="gimmeMainSpace smallLine">I'm a former philosophy student and coach turned software developer, an artist, and a music lover.</p>
+          <p className="gimmeMainSpace">I'm a former philosophy student and coach turned software developer, an artist, and a music lover.
           <br/>
-          <p className="gimmeMainSpace smallLine">I developed this website to showcase my resume and my art, as well as to learn React.js</p>
+          I developed this website to showcase my resume and my art, as well as to learn React.js
           <br/>
-          <p className="gimmeMainSpace smallLine" id="mainSpace">- You can contact me at -</p>
+          - You can contact me at -
           <br/>
           <p className="gimmeMainSpace smallLine" id="email">maxwell.bsmith0@gmail.com</p>
           <br/>
-          <p className="gimmeMainSpace smallLine" id="mainSpace">or by following any of the links below to my social media</p>
+          or by following any of the links below to my social media</p>
         </div>
         <div id="siteNav">
           <a href="https://github.com/mxwllsmth" target="_blank" rel="noopener noreferrer"><img src={githubLogo2} id="logo" alt="Github Logo"/></a>
