9ce70b7ed1bad1f5aa6e0e0e8d6fee7a8e57dbf6
Max Smith <megamaxbsmith@gmail.com>
2022-07-14 19:42:50 -0400

    Spelling mistake fixed

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -13,21 +13,21 @@ const Experience = props => {
 
         <div id="skillsContainer">
             <div className="skillJAVA reveal fade-in-two active"><p id="skill">Java</p></div>
-            <div className="skillUNIT reveal fade-in-two active"><p id="skill">Unit Testing</p></div>
             <div className="skillJS reveal fade-in-two active"><p id="skill">JavaScript</p></div>
             <div className="skillJSX reveal fade-in-two active"><p id="skill">JSX</p></div>
             <div className="skillPSQL reveal fade-in-two active"><p id="skill">PostgreSQL</p></div>
-            <div className="skillHTML reveal fade-in-two active"><p id="skill">HTML</p></div>
-            <div className="skillCSS reveal fade-in-two active"><p id="skill">CSS</p></div>
-            <div className="skillSPRING reveal fade-in-two active"><p id="skill">Spring Boot</p></div>
-            <div className="skillJSON reveal fade-in-two active"><p id="skill">JSON</p></div>
             <div className="skillREACT reveal fade-in-two active"><p id="skill">React.js</p></div>
             <div className="skillVUE reveal fade-in-two active"><p id="skill">Vue.js</p></div>
+            <div className="skillHTML reveal fade-in-two active"><p id="skill">HTML</p></div>
+            <div className="skillCSS reveal fade-in-two active"><p id="skill">CSS</p></div>
+            <div className="skillJSON reveal fade-in-two active"><p id="skill">JSON</p></div>
             <div className="skillGIT reveal fade-in-two active"><p id="skill">Git</p></div>
+            <div className="skillUNIT reveal fade-in-two active"><p id="skill">Unit Testing</p></div>
+            <div className="skillSPRING reveal fade-in-two active"><p id="skill">Spring Boot</p></div>
             <div className="skillINTELLIJ reveal fade-in-two active"><p id="skill">IntelliJ</p></div>
             <div className="skillVSCODE reveal fade-in-two active"><p id="skill">VS Code</p></div>
-            <div className="skillPOSTMAN reveal fade-in-two active"><p id="skill">Postman</p></div>
             <div className="skillPGADMIN reveal fade-in-two active"><p id="skill">pgAdmin 4</p></div>
+            <div className="skillPOSTMAN reveal fade-in-two active"><p id="skill">Postman</p></div>
         </div>
 
       </div>
@@ -45,7 +45,7 @@ const Experience = props => {
           environment had a lot to offer me, and through my time there I became an increasingly efficient worker, a stong team leader, and a valuable 
           addition to any team. My responsibilities ranged from coaching, where I would teach classes, communicate with parents, and train team members - to
           program management, where I would write lesson plans and lead our team of coaches - to customer service, where I dealt with delicate situations 
-          and helped to ensure the gym was functioning in an organized and efficient mannor.</p> 
+          and helped to ensure the gym was functioning in an organized and efficient manner.</p> 
           <p></p>
           <p>During my time at Bright Stars I :</p>
           <p></p>
